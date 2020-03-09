2ac452374960851a5175f7483b2eb1ea73b579d4
Ais53 <ais53ez53sa2@gmail.com>
2020-03-09 12:33:08 +0100

    Using bootstrap

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,18 +1,19 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 import time
 
 
 MAX_WAIT = 1.5
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
 
     def tearDown(self):
+        self.browser.refresh()
         self.browser.quit()
 
     def wait_for_row_in_list_table(self, row_text):
